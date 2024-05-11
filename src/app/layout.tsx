fec45a36bbd95859a8d7d531c00b555c20706393
Krzysztof Z <45123514+Zlvsky@users.noreply.github.com>
2024-05-11 11:37:51 +0200

    feat: added navbar animation & formatted code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
 export const metadata: Metadata = {
   title: 'Freelance Web Developer | Krzysztof Zaleski',
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,29 @@
 'use client'
-import { motion } from 'framer-motion'
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion'
+import { useState } from 'react'
 import Logo from './_components/Logo'
 import ModeToggle from './_components/ModeToggle'
 import NavLinks from './_components/NavLinks'
 import Socials from './_components/Socials'
 
 function Header() {
+  const { scrollY } = useScroll()
+  const [hidden, setHidden] = useState(false)
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    const previous = scrollY.getPrevious()
+    if (latest > previous! && latest > 100) setHidden(true)
+    else setHidden(false)
+  })
+
   return (
-    <motion.div
-      initial="visible"
-      transition={{ duration: 0.6, delay: 0.5 }}
+    <motion.nav
       variants={{
-        visible: { opacity: 1, y: 0 },
-        hidden: { opacity: 0, y: -100 }
+        visible: { y: 0 },
+        hidden: { y: '-100%' }
       }}
+      transition={{ duration: 0.25, ease: 'easeInOut' }}
+      animate={hidden ? 'hidden' : 'visible'}
       className="fixed left-0 right-0 top-4  z-20 mx-auto flex w-[96%] max-w-2xl items-center justify-between rounded-2xl border border-grayE8 bg-white/40 px-2 py-3 shadow-sm backdrop-blur-md dark:border-dark26 dark:bg-black/40 sm:px-5 sm:py-4 md:w-full"
     >
       <div className="flex flex-row items-center justify-between gap-4">
@@ -26,7 +36,7 @@ function Header() {
         <Socials />
         <ModeToggle />
       </div>
-    </motion.div>
+    </motion.nav>
   )
 }
 
